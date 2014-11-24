3371a4bf348557a5b03e84bcb24ba8aa26825a5c
Tellen Yu <tellen.yu@amlogic.com>
2014-11-24 10:43:19 +0800

    use default HDMI_PLUGGED action replace HDMI_HW_PLUDDED
    Change-Id: I4a85c7ffc751a785e73dc2fe7f5d8f9bc1540217

--- core/res/src/com/droidlogic/HdmiService.java
+++ core/res/src/com/droidlogic/HdmiService.java
@@ -25,15 +25,15 @@ public class HdmiService extends Service  {
     private static final String TAG = "HdmiService";
 
     /**
-     * Sticky broadcast of the current HDMI hw plugged state.
+     * Sticky broadcast of the current HDMI plugged state.
      */
-    public final static String ACTION_HDMI_HW_PLUGGED           = "android.intent.action.HDMI_HW_PLUGGED";
+    public final static String ACTION_HDMI_PLUGGED              = "android.intent.action.HDMI_PLUGGED";
 
     /**
-     * Extra in {@link #ACTION_HDMI_HW_PLUGGED} indicating the state: true if
-     * plugged in to HDMI hw, false if not.
+     * Extra in {@link #ACTION_HDMI_PLUGGED} indicating the state: true if
+     * plugged in to HDMI, false if not.
      */
-    public final static String EXTRA_HDMI_HW_PLUGGED_STATE      = "state";
+    public final static String EXTRA_HDMI_PLUGGED_STATE         = "state";
 
     /**
      *  broadcast of the current HDMI output mode changed.
@@ -56,12 +56,35 @@ public class HdmiService extends Service  {
     private boolean mHdmiHwPlugged = false;
     private boolean mHdmiPlugged = false;
 
+    final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            boolean plugged = intent.getBooleanExtra(EXTRA_HDMI_PLUGGED_STATE, false);
+
+            Log.i(TAG, "onReceive " + ACTION_HDMI_PLUGGED + " state:" + plugged);
+            setHdmiPlugged(plugged);
+        }
+    };
+
     @Override
     public void onCreate() {
         mContext = this;
 
+        Log.i(TAG, "onCreate ");
+
         mOutputMode = new OutputModeManager(this);
         mSystemControl = new SystemControlManager(this);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_HDMI_PLUGGED);
+        Intent intent = mContext.registerReceiver(mBroadcastReceiver, filter);
+        if (intent != null) {
+            boolean plugged = intent.getBooleanExtra(EXTRA_HDMI_PLUGGED_STATE, false);
+
+            Log.i(TAG, "registerReceiver " + ACTION_HDMI_PLUGGED + " state:" + plugged);
+            setHdmiPlugged(plugged);
+        }
 
         initHdmiState();
     }
@@ -81,59 +104,19 @@ public class HdmiService extends Service  {
         return null;
     }
 
-    private UEventObserver mHDMIObserver = new UEventObserver() {
-        @Override
-        public void onUEvent(UEventObserver.UEvent event) {
-            setHdmiHwPlugged("1".equals(event.get("SWITCH_STATE")));
-        }
-    };
+    private void setHdmiPlugged(boolean plugged) {
+        if (mHdmiPlugged != plugged) {
 
-    /*
-    private UEventObserver mHoldObserver = new UEventObserver() {
-        @Override
-        public void onUEvent(UEventObserver.UEvent event) {
-            Log.d("PhoneWindownManager","holdkey"+event.get("SWITCH_STATE"));
-            try {
-                IWindowManager wm = IWindowManager.Stub.asInterface(
-                    ServiceManager.getService(Context.WINDOW_SERVICE));
-                if("1".equals(event.get("SWITCH_STATE"))){
-                    wm.thawRotation();
-                }else{
-                    wm.freezeRotation(-1);//use current orientation
-                }
-            } catch (RemoteException exc) {
-                Log.w(TAG, "Unable to save auto-rotate setting");
-            }
-        }
-    };
-    */
+            Log.i(TAG, "HDMI state from " + (mHdmiPlugged?"plugged":"unplugged") +
+                " -> " + (plugged?"plugged":"unplugged"));
 
-    void setHdmiHwPlugged(boolean plugged) {
-        if (mHdmiHwPlugged != plugged) {
-            Log.i(TAG, "setHdmiHwPlugged " + plugged);
-            mHdmiHwPlugged = plugged;
-
+            mHdmiPlugged = plugged;
             if (mSystemControl.getPropertyBoolean("ro.platform.has.mbxuimode", false)){
                 if (plugged)
                     mOutputMode.setHdmiPlugged();
                 else
                     mOutputMode.setHdmiUnPlugged();
             }
-
-            /*
-            Intent intent = new Intent(WindowManagerPolicy.ACTION_HDMI_HW_PLUGGED);
-            intent.addFlags(Intent.FLAG_RECEIVER_REGISTERED_ONLY_BEFORE_BOOT);
-            intent.putExtra(EXTRA_HDMI_HW_PLUGGED_STATE, plugged);
-            mContext.sendStickyBroadcastAsUser(intent, UserHandle.OWNER);
-
-            if (mSystemControl.getPropertyBoolean("ro.vout.dualdisplay", false)) {
-                setDualDisplay(plugged);
-
-                Intent it = new Intent(WindowManagerPolicy.ACTION_HDMI_PLUGGED);
-                it.addFlags(Intent.FLAG_RECEIVER_REGISTERED_ONLY_BEFORE_BOOT);
-                it.putExtra(WindowManagerPolicy.EXTRA_HDMI_PLUGGED_STATE, plugged);
-                mContext.sendStickyBroadcastAsUser(it, UserHandle.OWNER);
-            }*/
         }
     }
 
@@ -145,58 +128,17 @@ public class HdmiService extends Service  {
             if (mSystemControl.getPropertyBoolean("ro.platform.has.mbxuimode", false)){
                 mOutputMode.initOutputMode();
             }
-            mHDMIObserver.startObserving("DEVPATH=/devices/virtual/switch/hdmi");
 
+            /*
             final String filename = "/sys/class/switch/hdmi/state";
             String state = mSystemControl.readSysFs(filename);
 
-            Log.i(TAG, "initHdmiState :" + state);
+            Log.i(TAG, "HDMI plugged :" + state);
 
             plugged = (0 != Integer.parseInt(state));
-        }
-
-        /*
-        mHdmiHwPlugged = plugged;
-        if (!mSystemControl.getPropertyBoolean("ro.vout.dualdisplay", false)) {
-            if (getCurDisplayMode().equals("panel") ||
-                !plugged ||
-                SystemProperties.getBoolean("ro.platform.has.mbxuimode", false)) {
-                plugged = false;
-            }
-        }
-
-        if (mSystemControl.getPropertyBoolean("ro.vout.dualdisplay", false)) {
-            setDualDisplay(plugged);
-        }
-
-        if (mSystemControl.getPropertyBoolean("ro.vout.dualdisplay2", false)) {
-            plugged = false;
-            setDualDisplay(plugged);
-        }
-
-        Intent it = new Intent(WindowManagerPolicy.ACTION_HDMI_PLUGGED);
-        it.addFlags(Intent.FLAG_RECEIVER_REGISTERED_ONLY_BEFORE_BOOT);
-        it.putExtra(WindowManagerPolicy.EXTRA_HDMI_PLUGGED_STATE, plugged);
-        mContext.sendStickyBroadcastAsUser(it, UserHandle.OWNER);
-        */
-    }
-
-    /*
-    private void initHoldkeyState( IWindowManager windowManager ) {
-        boolean hold = false;
-        if (new File("/sys/devices/virtual/switch/hold_key/state").exists()) {
-            mHoldObserver.startObserving("DEVPATH=/devices/virtual/switch/hold_key");
-            final String filename = "/sys/class/switch/hold_key/state";
-
-            hold = (0 != Integer.parseInt(mSystemControl.readSysFs(filename)));
-            if(hold){
-                windowManager.thawRotation();
-            }else{
-                windowManager.freezeRotation(-1);//use current orientation
-            }
+            */
         }
     }
-    */
 
     private void setDualDisplay(boolean hdmiPlugged) {
         String isCameraBusy = mSystemControl.getPropertyString("camera.busy", "0");
