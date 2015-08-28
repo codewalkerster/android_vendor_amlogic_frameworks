7ed8d59aa205e3656da91a0a90cd97103107b1d3
Frank Chen <frank.chen@amlogic.com>
2015-08-28 18:14:16 +0800

    PD#111909 dig: redesign dig
    dig with build as library, and run as a thread in vold
    
    Change-Id: I33c55a0ccbc8720ef00e0f4b93afe05aadd22332

--- services/data_integrity_guard/Android.mk
+++ services/data_integrity_guard/Android.mk
@@ -1,14 +1,17 @@
 # Copyright 2005 The Android Open Source Project
 
 LOCAL_PATH:= $(call my-dir)
+
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES:= \
-	data_integrity_guard.c
+	DigManager.cpp \
+        main.cpp
 
-LOCAL_CFLAGS += -DUSE_KERNEL_LOG
 
-LOCAL_MODULE:= dig
+LOCAL_CFLAGS += -DUSE_KERNEL_LOG -DDIG_TEST
+
+LOCAL_MODULE:= digtest
 
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 
@@ -17,7 +20,34 @@ LOCAL_STATIC_LIBRARIES := libcutils libc\
 	libext4_utils_static \
 	libsparse_static \
 	libz\
-        libselinux
+        libselinux\
+        liblog
+
+LOCAL_C_INCLUDES += system/extras/ext4_utils \
+                    system/vold
 
 include $(BUILD_EXECUTABLE)
 
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+        DigManager.cpp
+
+LOCAL_CFLAGS += -DUSE_KERNEL_LOG
+
+LOCAL_MODULE:= libdig
+
+LOCAL_C_INCLUDES += system/extras/ext4_utils \
+                    system/vold
+
+LOCAL_SHARED_LIBRARIES := libcutils libc\
+        libcrypto \
+        libext4_utils \
+        libsparse \
+        libz\
+        libselinux \
+        libsysutils
+
+LOCAL_STATIC_LIBRARIES:= libvold
+
+include $(BUILD_SHARED_LIBRARY)
