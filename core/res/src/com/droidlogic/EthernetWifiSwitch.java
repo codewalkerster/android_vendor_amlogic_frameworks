ddec624c18dbd5282a28be077ad08a3910c4b038
Zhiwei Gong <zhiwei.gong@amlogic.com>
2015-12-04 16:37:56 +0800

    change a method to get pppoe state
    Change-Id: I4ec89857aa0f2b5e62740cb41ecd45a3697a8056

--- core/res/src/com/droidlogic/EthernetWifiSwitch.java
+++ core/res/src/com/droidlogic/EthernetWifiSwitch.java
@@ -7,21 +7,21 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
 import android.util.Log;
+import com.amlogic.pppoe.PppoeOperation;
 
 public class EthernetWifiSwitch extends BroadcastReceiver {
-    private static final String TAG         = "EthernetWifiSwitch";
+    private static final String TAG = "EthernetWifiSwitch";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
+        PppoeOperation mPppoeOperation = new PppoeOperation();
         Log.i(TAG, "action: " + action);
-
         if (ConnectivityManager.CONNECTIVITY_ACTION.equals(action)) {
             WifiManager wm = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
             ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
             NetworkInfo ethInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_ETHERNET);
-            NetworkInfo pppoeInfo = cm.getNetworkInfo(0/*ConnectivityManager.TYPE_PPPOE*/);
-            if (pppoeInfo.getState() == NetworkInfo.State.CONNECTED || ethInfo.getState() == NetworkInfo.State.CONNECTED) {
+            if (ethInfo.getState() == NetworkInfo.State.CONNECTED || mPppoeOperation.status("eth0") == PppoeOperation.PPP_STATUS_CONNECTED) {
                 int wifiState = wm.getWifiState();
                 if ((wifiState == WifiManager.WIFI_STATE_ENABLING) ||
                     (wifiState == WifiManager.WIFI_STATE_ENABLED)) {
