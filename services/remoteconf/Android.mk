aa746b3f7980e7869985af471ea8df272f3566c2
Zhigang Yu <zhigang.yu@amlogic.com>
2016-03-16 11:15:42 +0800

    PD#120782 build static lib remotecfg
    Change-Id: I504e1ff7a8669532c0e41f331fd81854cc0d07cf

--- services/remoteconf/Android.mk
+++ services/remoteconf/Android.mk
@@ -11,6 +11,14 @@ LOCAL_STATIC_LIBRARIES := libcutils libc liblog
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
+LOCAL_SRC_FILES:= remote_init.c config.c parsefile.c
+LOCAL_MODULE := libremotecfg_static
+LOCAL_MODULE_TAGS := optional
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_STATIC_LIBRARIES := libcutils libc liblog
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= irkey.c
 LOCAL_MODULE := keytest
 LOCAL_MODULE_TAGS := optional
