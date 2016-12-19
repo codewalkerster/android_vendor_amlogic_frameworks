6efd85bf3a08b3c4443fe4ff7b7c28cea52bbc46
Luke Go <luke.go@hardkernel.com>
2016-12-19 10:59:15 +0900

    Revert "PD#111909 dig: redesign dig"
    This reverts commit 7ed8d59aa205e3656da91a0a90cd97103107b1d3.
    
    Conflicts:
    	services/data_integrity_guard/DigManager.cpp
    
    Change-Id: I90e8ec90ad5f3deffe65ace3161f93f2471e2c06

--- services/data_integrity_guard/Android.mk
+++ services/data_integrity_guard/Android.mk
@@ -1,17 +1,14 @@
 # Copyright 2005 The Android Open Source Project
 
 LOCAL_PATH:= $(call my-dir)
-
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES:= \
-	DigManager.cpp \
-        main.cpp
+	data_integrity_guard.c
 
+LOCAL_CFLAGS += -DUSE_KERNEL_LOG
 
-LOCAL_CFLAGS += -DUSE_KERNEL_LOG -DDIG_TEST
-
-LOCAL_MODULE:= digtest
+LOCAL_MODULE:= dig
 
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 
@@ -20,34 +17,7 @@ LOCAL_STATIC_LIBRARIES := libcutils libc\
 	libext4_utils_static \
 	libsparse_static \
 	libz\
-        libselinux\
-        liblog
-
-LOCAL_C_INCLUDES += system/extras/ext4_utils \
-                    system/vold
+        libselinux
 
 include $(BUILD_EXECUTABLE)
 
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES:= \
-        DigManager.cpp
-
-LOCAL_CFLAGS += -DUSE_KERNEL_LOG
-
-LOCAL_MODULE:= libdig
-
-LOCAL_C_INCLUDES += system/extras/ext4_utils \
-                    system/vold
-
-LOCAL_SHARED_LIBRARIES := libcutils libc\
-        libcrypto \
-        libext4_utils \
-        libsparse \
-        libz\
-        libselinux \
-        libsysutils
-
-LOCAL_STATIC_LIBRARIES:= libvold
-
-include $(BUILD_SHARED_LIBRARY)
